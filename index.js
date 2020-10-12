5f3bc287151a914d40ecbac1042dbe9b0dc866c4
Igor.Mikhailov <iomihailov@gmail.com>
2020-10-12 16:53:49 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import MapsTest from './MapsTest';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => MapsTest);
