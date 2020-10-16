781a2d84421eda51032125e702a421fc0a243a65
Igor.Mikhailov <iomihailov@gmail.com>
2020-10-16 17:55:41 +0300

    first commit

--- src/MapScreen.js
+++ src/MapScreen.js
@@ -6,9 +6,9 @@ import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'; // remove PROVIDER_G
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
-    height: 400,
-    width: 400,
-    // flex: 1,
+    // height: 400,
+    // width: 400,
+    flex: 1,
     justifyContent: 'flex-end',
     alignItems: 'center',
   },
@@ -20,7 +20,7 @@ const styles = StyleSheet.create({
 export default () => (
   <View style={styles.container}>
     <MapView
-      provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+      // provider={PROVIDER_GOOGLE} // remove if not using Google Maps
       style={styles.map}
       region={{
         latitude: 37.78825,
